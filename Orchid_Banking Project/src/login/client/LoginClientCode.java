03f3a9b377f688b65113320ddf42e5400143965d
anilchinchawade <anilchinchawade1@gmail.com>
2021-02-02 22:44:42 +0530

    After Correcting LoginClientCode

--- Orchid_Banking Project/src/login/client/LoginClientCode.java
+++ Orchid_Banking Project/src/login/client/LoginClientCode.java
@@ -8,12 +8,12 @@ public class LoginClientCode
 	public static void main(String[] args) 
 	{
 		UserValidation services = new UserValidation();
-		services.SignUp("Anil", "anil@123", "What is your name");
+		services.SignUp("Anil", "anil@123", "What is your name","Anil");
 		
-		String msg = services.SignIn("Anil", "anil@12");
+		String msg = services.SignIn("Anil", "anil@123");
 		System.out.println(msg);
 		
-		msg = services.ForgetPassword("What is your Name");
+		msg = services.ForgetPassword("What is your name","Anil");
 		System.out.println(msg);
 		
 		msg = services.updatePassword("Anil", "anil@123", "anil@789");
